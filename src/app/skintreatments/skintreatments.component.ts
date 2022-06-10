9cbd4a353b322178dc5a3033071b95ac96938a9e
apurva-tambe <81083468+apurva-tambe@users.noreply.github.com>
2022-06-10 18:35:35 +0530

    new code for skin treatments

--- src/app/skintreatments/skintreatments.component.ts
+++ src/app/skintreatments/skintreatments.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skintreatments.component.css']
 })
 export class SkintreatmentsComponent implements OnInit {
-
+  public laser = false;
+  show=true;
+  showMore1 = false;
   constructor() { }
-
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
 
+  stars: number[] = [1, 2, 3, 4, 5];
+selectedValue: number = 0;
+isMouseover = true;
+
+countStar(star: number) {
+  this.isMouseover = false;
+  this.selectedValue = star;
+}
+
+ addClass(star: number) {
+  if (this.isMouseover) {
+    this.selectedValue = star;
+  }
+ }
+
+ removeClass() {
+   if (this.isMouseover) {
+      this.selectedValue = 0;
+   }
+  }
 }
