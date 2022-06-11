2463ac0cce97f2db178d98e5741e15d2462c260a
apurva-tambe <81083468+apurva-tambe@users.noreply.github.com>
2022-06-11 15:32:48 +0530

    videos added

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -6,7 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-
+  
+  stars: number[] = [1, 2, 3, 4, 5];
+  selectedValue: number = 0;
+  isMouseover = true;
+  
+  countStar(star: number) {
+    this.isMouseover = false;
+    this.selectedValue = star;
+  }
+  
+   addClass(star: number) {
+    if (this.isMouseover) {
+      this.selectedValue = star;
+    }
+   }
+  
+   removeClass() {
+     if (this.isMouseover) {
+        this.selectedValue = 0;
+     }
+   }
   constructor() { }
 
   ngOnInit(): void {
--- src/app/laserhairreduction/laserhairreduction.component.ts
+++ src/app/laserhairreduction/laserhairreduction.component.ts
@@ -9,7 +9,26 @@ export class LaserhairreductionComponent implements OnInit {
   public laser = false;
   show=true;
   showMore1 = false;
-
+  stars: number[] = [1, 2, 3, 4, 5];
+  selectedValue: number = 0;
+  isMouseover = true;
+  
+  countStar(star: number) {
+    this.isMouseover = false;
+    this.selectedValue = star;
+  }
+  
+   addClass(star: number) {
+    if (this.isMouseover) {
+      this.selectedValue = star;
+    }
+   }
+  
+   removeClass() {
+     if (this.isMouseover) {
+        this.selectedValue = 0;
+     }
+   }
   constructor() { }
 
   ngOnInit(): void {
